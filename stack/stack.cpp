5dc90f9a4ef2a67bb2906a78f7d526b7f7c86424
bondarevVV <90005849+bondarevVV@users.noreply.github.com>
2022-04-09 10:48:34 +0300

    First commit

---
+++ stack/stack.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int max(int* arr, size_t size) {
+	if (size == 1)
+		return *arr;
+	const size_t count = size / 2;
+	return max(max(arr, count), max(arr + count, size - count));
+}
+
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	const size_t n = 5;
+	int arr[n];
+	srand((unsigned)time(nullptr));
+
+	for (int& item : arr) {
+		item = rand() % 50;
+		cout << item << ' ';
+	}
+
+	cout << '\n' << "Макс. элемент массива: " << max(arr, n) << '\n';
+	return 0;
+}
